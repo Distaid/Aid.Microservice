7a09ea06ba43ec28ee1f643ce0b786d1b892f512
distaid <sinyak00pavel@gmail.com>
2025-07-20 11:48:19 +0300

    Update AidMicroserviceClientExtensions

--- src/Aid.Microservice.Client.AspNetCore/AidMicroserviceClientExtensions.cs
+++ src/Aid.Microservice.Client.AspNetCore/AidMicroserviceClientExtensions.cs
@@ -7,6 +7,14 @@ namespace Aid.Microservice.Client.AspNetCore;
 
 public static class AidMicroserviceClientExtensions
 {
+    /// <summary>
+    /// Adds RpcClient to the container with RabbitMq configuration from appsettings.json.
+    /// </summary>
+    /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /></param>
+    /// <returns>The same instance of the <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> for chaining</returns>
+    /// <remarks>
+    /// Needs RabbitMqConfiguration section in appsettings.json that represent <see cref="Aid.Microservice.Client.Configuration.RabbitMqConfiguration" />. Register as Scoped service.
+    /// </remarks>
     public static IServiceCollection AddMicroserviceClient(this IServiceCollection services)
     {
         services.TryAddScoped<RpcClient>(sp =>
@@ -20,6 +28,27 @@ public static class AidMicroserviceClientExtensions
                 config["Username"]!,
                 config["Password"]!);
         });
+        
+        return services;
+    }
+    
+    /// <summary>
+    /// Adds RpcClient to the container with RabbitMq configuration.
+    /// </summary>
+    /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /></param>
+    /// <param name="configuration"><see cref="Aid.Microservice.Client.Configuration.RabbitMqConfiguration" /> instance to initialize connection to RabbitMq</param>
+    /// <returns>The same instance of the <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> for chaining</returns>
+    /// <remarks>
+    /// Register as Scoped service
+    /// </remarks>
+    public static IServiceCollection AddMicroserviceClient(this IServiceCollection services, RabbitMqConfiguration configuration)
+    {
+        services.TryAddScoped<RpcClient>(sp => new RpcClient(
+            configuration.Hostname,
+            configuration.Port,
+            configuration.Username,
+            configuration.Password)
+        );
         
         return services;
     }
