4e3840329d0c60d94f2139429c45c22c11d4a9ff
distaid <sinyak00pavel@gmail.com>
2026-01-18 22:01:24 +0300

    remove unused using

--- src/Aid.Microservice.Client/RpcClient.cs
+++ src/Aid.Microservice.Client/RpcClient.cs
@@ -3,7 +3,6 @@ using System.Text.Json;
 using Aid.Microservice.Client.Infrastructure;
 using Aid.Microservice.Client.Models;
 using Aid.Microservice.Shared.Interfaces;
-using Aid.Microservice.Shared.Models;
 using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
