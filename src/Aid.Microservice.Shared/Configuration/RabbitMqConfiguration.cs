9dbc937eb1680f4c52e140a00a4c828e9c6db9a4
distaid <sinyak00pavel@gmail.com>
2026-04-10 21:30:44 +0300

    feat(shared): add DataAnnotations validation

--- src/Aid.Microservice.Shared/Configuration/RabbitMqConfiguration.cs
+++ src/Aid.Microservice.Shared/Configuration/RabbitMqConfiguration.cs
@@ -1,13 +1,28 @@
-﻿namespace Aid.Microservice.Shared.Configuration;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aid.Microservice.Shared.Configuration;
 
 public class RabbitMqConfiguration
 {
+    [Required]
     public string Hostname { get; set; } = "localhost";
+
+    [Range(1, 65535)]
     public int Port { get; set; } = 5672;
+
+    [Required]
     public string Username { get; set; } = "guest";
+
+    [Required]
     public string Password { get; set; } = "guest";
+
     public string? ExchangeName { get; set; }
+
+    [Range(0, int.MaxValue)]
     public int RetryCount { get; set; } = 3;
+
+    [Range(1, int.MaxValue)]
     public int RecoveryInterval { get; set; } = 5;
+
     public bool DeleteExchangesOnShutdown { get; set; }
 }
